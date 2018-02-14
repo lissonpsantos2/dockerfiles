2f59f5c4f3040f988a5e849d47b3176c3a7a3e8c
Alisson Pereira <lissonpsantos2@gmail.com>
2018-02-14 09:53:25 -0300

    cria o shellscript

---
+++ ubuntu/selectphp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SELECTED=$1
+
+if [ "$SELECTED"=="7.1" ]; then
+  update-alternatives --set php /usr/bin/php7.1
+  a2dismod php7.0
+  a2dismod php5.6
+  a2enmod php7.1
+  systemctl restart apache2
+	echo "PHP 7.1 ACTIVATED!"
+  exit 0
+fi
+
+if [ "$SELECTED"=="7.0" ]; then
+  update-alternatives --set php /usr/bin/php7.0
+  a2dismod php7.1
+  a2dismod php5.6
+  a2enmod php7.0
+  systemctl restart apache2
+	echo "PHP 7.0 ACTIVATED!"
+  exit 0
+fi
+
+if [ "$SELECTED"=="5.6" ]; then
+  update-alternatives --set php /usr/bin/php5.6
+  a2dismod php7.1
+  a2dismod php7.0
+  a2enmod php5.6
+  systemctl restart apache2
+	echo "PHP 5.6 ACTIVATED!"
+  exit 0
+fi
