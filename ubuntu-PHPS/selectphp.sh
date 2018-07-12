9384d9a73b79dcc2e9d0ba29cf73bb8eac30e78b
esron <esron.dtamar@gmail.com>
2018-07-12 17:34:21 -0300

    Instalar php 7.2

--- ubuntu-PHPS/selectphp.sh
+++ ubuntu-PHPS/selectphp.sh
@@ -2,8 +2,20 @@
 
 SELECTED=$1
 
+if [ "$SELECTED" = "7.2" ]; then
+  update-alternatives --set php /usr/bin/php7.2
+  a2dismod php7.1
+  a2dismod php7.0
+  a2dismod php5.6
+  a2enmod php7.2
+  service apache2 restart
+	echo "PHP 7.1 ACTIVATED!"
+  exit 0
+fi
+
 if [ "$SELECTED" = "7.1" ]; then
   update-alternatives --set php /usr/bin/php7.1
+  a2dismod php7.2
   a2dismod php7.0
   a2dismod php5.6
   a2enmod php7.1
@@ -14,6 +26,7 @@ fi
 
 if [ "$SELECTED" = "7.0" ]; then
   update-alternatives --set php /usr/bin/php7.0
+  a2dismod php7.2
   a2dismod php7.1
   a2dismod php5.6
   a2enmod php7.0
@@ -24,6 +37,7 @@ fi
 
 if [ "$SELECTED" = "5.6" ]; then
   update-alternatives --set php /usr/bin/php5.6
+  a2dismod php7.2
   a2dismod php7.1
   a2dismod php7.0
   a2enmod php5.6
