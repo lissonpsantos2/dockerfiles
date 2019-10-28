7306623dfb2ee76d38fc13afa4249c0a311e1203
Alisson Pereira <lissonpsantos2@gmail.com>
2019-10-27 22:20:36 -0300

    Adiciona as dependencias do php7.3 ao projeto

--- ubuntu-PHPS/selectphp.sh
+++ ubuntu-PHPS/selectphp.sh
@@ -6,6 +6,7 @@ if [ "$SELECTED" = "7.2" ]; then
   update-alternatives --set php /usr/bin/php7.2
   a2dismod php7.1
   a2dismod php5.6
+  a2dismod php7.3
   a2enmod php7.2
   service apache2 restart
 	echo "PHP 7.2 ACTIVATED!"
@@ -16,6 +17,7 @@ if [ "$SELECTED" = "7.1" ]; then
   update-alternatives --set php /usr/bin/php7.1
   a2dismod php7.2
   a2dismod php5.6
+  a2dismod php7.3
   a2enmod php7.1
   service apache2 restart
 	echo "PHP 7.1 ACTIVATED!"
@@ -26,8 +28,20 @@ if [ "$SELECTED" = "5.6" ]; then
   update-alternatives --set php /usr/bin/php5.6
   a2dismod php7.2
   a2dismod php7.1
+  a2dismod php7.3
   a2enmod php5.6
   service apache2 restart
 	echo "PHP 5.6 ACTIVATED!"
+  exit 0
+fi
+
+if [ "$SELECTED" = "7.3" ]; then
+  update-alternatives --set php /usr/bin/php7.3
+  a2dismod php7.2
+  a2dismod php7.1
+  a2dismod php5.6
+  a2enmod php7.3
+  service apache2 restart
+	echo "PHP 7.3 ACTIVATED!"
   exit 0
 fi
