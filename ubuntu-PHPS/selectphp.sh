da3023725b597b65118cda72d17fecf3a7778c52
Alisson Pereira <lssonpsantos2@gmail.com>
2020-10-08 08:14:34 -0300

    Adiciona versao nova do ubuntu basic e adiciona o php7.4 no ubuntuphps

--- ubuntu-PHPS/selectphp.sh
+++ ubuntu-PHPS/selectphp.sh
@@ -2,46 +2,62 @@
 
 SELECTED=$1
 
-if [ "$SELECTED" = "7.2" ]; then
-  update-alternatives --set php /usr/bin/php7.2
-  a2dismod php7.1
-  a2dismod php5.6
-  a2dismod php7.3
-  a2enmod php7.2
-  service apache2 restart
-	echo "PHP 7.2 ACTIVATED!"
-  exit 0
-fi
-
-if [ "$SELECTED" = "7.1" ]; then
-  update-alternatives --set php /usr/bin/php7.1
-  a2dismod php7.2
-  a2dismod php5.6
-  a2dismod php7.3
-  a2enmod php7.1
-  service apache2 restart
-	echo "PHP 7.1 ACTIVATED!"
-  exit 0
-fi
-
 if [ "$SELECTED" = "5.6" ]; then
   update-alternatives --set php /usr/bin/php5.6
-  a2dismod php7.2
-  a2dismod php7.1
-  a2dismod php7.3
   a2enmod php5.6
+  a2dismod php7.1
+  a2dismod php7.2
+  a2dismod php7.3
+  a2dismod php7.4
   service apache2 restart
 	echo "PHP 5.6 ACTIVATED!"
   exit 0
 fi
 
+if [ "$SELECTED" = "7.1" ]; then
+  update-alternatives --set php /usr/bin/php7.1
+  a2dismod php5.6
+  a2enmod php7.1
+  a2dismod php7.2
+  a2dismod php7.3
+  a2dismod php7.4
+  service apache2 restart
+	echo "PHP 7.1 ACTIVATED!"
+  exit 0
+fi
+
+if [ "$SELECTED" = "7.2" ]; then
+  update-alternatives --set php /usr/bin/php7.2
+  a2dismod php5.6
+  a2dismod php7.1
+  a2enmod php7.2
+  a2dismod php7.3
+  a2dismod php7.4
+  service apache2 restart
+	echo "PHP 7.2 ACTIVATED!"
+  exit 0
+fi
+
 if [ "$SELECTED" = "7.3" ]; then
   update-alternatives --set php /usr/bin/php7.3
-  a2dismod php7.2
-  a2dismod php7.1
   a2dismod php5.6
+  a2dismod php7.1
+  a2dismod php7.2
   a2enmod php7.3
+  a2dismod php7.4
   service apache2 restart
 	echo "PHP 7.3 ACTIVATED!"
+  exit 0
+fi
+
+if [ "$SELECTED" = "7.4" ]; then
+  update-alternatives --set php /usr/bin/php7.4
+  a2dismod php5.6
+  a2dismod php7.1
+  a2dismod php7.2
+  a2dismod php7.3
+  a2enmod php7.4
+  service apache2 restart
+	echo "PHP 7.4 ACTIVATED!"
   exit 0
 fi
